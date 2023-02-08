41e66b35f83cb2c77401663f8ed3f9f633cbbe90
David Bennet <you@example.com>
2023-02-08 13:02:39 -0300

    criação dos novos hábitos conectada com o backEnd

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -3,15 +3,20 @@ import clsx from 'clsx'
 import { ProgressBar } from './ProgressBar'
 import * as Checkbox from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
+import dayjs from 'dayjs'
 
 
 interface HabitDayProps {
-    completed: number,
-    amount: number,
+    date: Date,
+    completed?: number,
+    amount?: number,
 }
 
-export function HabitDay(props: HabitDayProps) {
-    const completedPercentage = Math.round((props.completed / props.amount) * 100)
+export function HabitDay({completed = 0, amount = 0, date}: HabitDayProps) {
+    const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
+
+    const dayAndAmount = dayjs(date).format('DD/MM')
+    const dayOfWeek = dayjs(date).format('dddd')
 
     return(
         <Popover.Root>
@@ -28,11 +33,11 @@ export function HabitDay(props: HabitDayProps) {
                 
                 <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
                     <span className='font-semibold text-zinc-400 '>
-                        sexta-feira    
+                        {dayOfWeek}    
                     </span>
                     
                     <span className='mt-1 font-extrabold leading-tight text-3xl'>
-                        03/02
+                        {dayAndAmount}
                     </span>
 
                     <ProgressBar progress={completedPercentage}/>
